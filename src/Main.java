8c6b88d43f02229159ae05afbac4319d72069fb5
Allina15 <140904065+Allina15@users.noreply.github.com>
2023-08-14 17:34:39 +0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+    Product[]products = new Product[]{
+            new Milk("Milk",60),
+            new Milk("Milk",60),
+            new Milk("Milk",60),
+            new Milk("Milk",60),
+            new Milk("Milk",60),
+            new Corn("Corn",150),
+            new Corn("Corn",150),
+            new Corn("Corn",150),
+            new Corn("Corn",150),
+            new Corn("Corn",150),
+            new Fish("Fish",20),
+            new Stew("Stew",180),
+            new Stew("Stew",180),
+            new Stew("Stew",180),
+            new Stew("Stew",180),
+            new Stew("Stew",180),
+            new Salt("Salt",0),
+            new Salt("Salt",0),
+            new Salt("Salt",0),
+            new Salt("Salt",0),
+            new Salt("Salt",0),
+            };
+        Stor stor = new Stor();
+        stor.setProducts(products);
+        stor.doInspection();
+    }
+}
